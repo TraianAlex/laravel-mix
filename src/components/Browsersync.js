72f8bcf0aa147916a08bb30fb7d718f735181af1
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-02-26 17:33:54 -0500

    wip

--- src/components/Browsersync.js
+++ src/components/Browsersync.js
@@ -20,36 +20,39 @@ class Browsersync {
     }
 
     webpackPlugins() {
-        // Add support for browser reloading with BrowserSync.
         if (Mix.isUsing("browserSync")) {
             let BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
-            return new BrowserSyncPlugin(
-                Object.assign(
-                    {
-                        host: "localhost",
-                        port: 3000,
-                        proxy: "app.dev",
-                        files: [
-                            "app/**/*.php",
-                            "resources/views/**/*.php",
-                            "public/js/**/*.js",
-                            "public/css/**/*.css"
-                        ],
-                        snippetOptions: {
-                            rule: {
-                                match: /(<\/body>|<\/pre>)/i,
-                                fn: function(snippet, match) {
-                                    return snippet + match;
-                                }
+            return new BrowserSyncPlugin(this.browserSyncConfig());
+        }
+    }
+
+    browserSyncConfig() {
+        return (
+            Object.assign(
+                {
+                    host: "localhost",
+                    port: 3000,
+                    proxy: "app.test",
+                    files: [
+                        "app/**/*.php",
+                        "resources/views/**/*.php",
+                        "public/js/**/*.js",
+                        "public/css/**/*.css"
+                    ],
+                    snippetOptions: {
+                        rule: {
+                            match: /(<\/body>|<\/pre>)/i,
+                            fn: function(snippet, match) {
+                                return snippet + match;
                             }
                         }
-                    },
-                    Config.browserSync
-                ),
-                { reload: false }
-            );
-        }
+                    }
+                },
+                Config.browserSync
+            ),
+            { reload: false }
+        );
     }
 }
 
